eebaa4456510afd3d6ff2e5d47679ae6825bc18a
Fabio Andrade <fabioandrade697@gmail.com>
2024-08-27 20:58:06 -0300

    three

--- src/pages/Inicio/components/QuemSomos/index.tsx
+++ src/pages/Inicio/components/QuemSomos/index.tsx
@@ -8,7 +8,7 @@ const QuemSomos = () => {
                 <img src={HomemCozinhando} alt="Homem cozinhando" />
                 <div>
                     <h4 className="text-2xl">Nossa História:</h4>
-                    <p>A Burger Boss nasceu da paixão por hambúrgueres suculentos e ingredientes frescos. Tudo começou quando nosso fundador, Bob, decidiu transformar sua receita secreta de hambúrguer em um negócio. Com muito amor pela gastronomia e uma pitada de ousadia, a Burger Boss ganhou vida.</p>
+                    <p>O Burger Boss nasceu da paixão por hambúrgueres suculentos e ingredientes frescos. Tudo começou quando nosso fundador, Bob, decidiu transformar sua receita secreta de hambúrguer em um negócio. Com muito amor pela gastronomia e uma pitada de ousadia, a Burger Boss ganhou vida.</p>
                     <h4 className="text-2xl pt-10">Nossa Missão:</h4>
                     <p>Nosso objetivo é simples, unir as pessoas em torno de uma mesa deliciosa. Acreditamos que um bom hambúrguer pode fazer isso acontecer. Queremos ser o lugar onde amigos se encontram, famílias celebram e amantes de comida se deliciam.</p>
                 </div>
