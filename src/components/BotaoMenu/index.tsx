b843c39deca7b522cb046529c0b7e3afc6a6cef1
Fabio Andrade <fabioandrade697@gmail.com>
2024-08-31 16:03:42 -0300

    Changing menu button

--- src/components/BotaoMenu/index.tsx
+++ src/components/BotaoMenu/index.tsx
@@ -1,8 +1,9 @@
+import MenuHamburguer from "/icons/hamburguer.png";
+
 const BotaoMenu = () => {
     return (
-        <div 
-            className="flex flex-col fixed top-0 right-0 z-40 p-5"
-        >
+        <div className="flex flex-col fixed bottom-40 right-20 z-40 p-5 bg-white rounded-full">
+            <img src={MenuHamburguer} alt="Menu hamburguer" className="w-14 h-14"/>
         </div>
     )
 }
--- src/pages/InicioSistema/index.tsx
+++ src/pages/InicioSistema/index.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import BotaoMenu from "../../components/BotaoMenu";
 import Menu from "../../components/Menu";
 import HeaderInicio from "../Inicio/components/HeaderInicio";
 
 const InicioSistema = () => {
+    const [menu, setMenu] = useState(false);
+    const mostrarMenu = () => setMenu(!menu);
+
     return(
         <main className="h-dvh bg-hamburguer bg-no-repeat bg-right-top">
             <HeaderInicio />
-            <Menu />
+            {menu && <Menu/>}
             <BotaoMenu />
             <div className="pt-28 pb-32 w-1/2 m-32">
                 <h2 className="font-bb text-8xl pb-6">Burguer Boss</h2>
@@ -16,3 +20,5 @@ const InicioSistema = () => {
     )
 }
 export default InicioSistema;
+
+//aoMenuMostrado={mostrarMenu}
